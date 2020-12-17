a0897ee5b4451bdbb6ea43ea2fe5382a8332cc5d
wnshd <wnshddlgpejddl@hanmail.net>
2020-12-17 16:01:09 +0900

    주혜정_값넘기기

--- app/src/main/java/com/example/andorid_team4_bmi/MainActivity.java
+++ app/src/main/java/com/example/andorid_team4_bmi/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.andorid_team4_bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
